1bfa3ee0689b45d61f65370189109bb41ca5db7a
i5un <35351414+i5un@users.noreply.github.com>
2018-10-29 13:55:32 -0400

    attempt to read files from direc

--- NewYork/GameManager.cpp
+++ NewYork/GameManager.cpp
@@ -10,3 +10,15 @@ GameManager::GameManager()
 GameManager::~GameManager()
 {
 }
+
+void GameManager::StartGame() {
+	WIN32_FIND_DATA data;
+	HANDLE hFind = FindFirstFile("C:\\semester2", &data);      // DIRECTORY
+
+	if (hFind != INVALID_HANDLE_VALUE) {
+		do {
+			std::cout << data.cFileName << std::endl;
+		} while (FindNextFile(hFind, &data));
+		FindClose(hFind);
+	}
+}
--- NewYork/GameManager.h
+++ NewYork/GameManager.h
@@ -1,7 +1,15 @@
 #pragma once
+#include<string>
+#include<iostream>
+#include<Windows.h>
+
 class GameManager
 {
 public:
+	class Game {
+
+	};
+	void StartGame();
 	GameManager();
 	~GameManager();
 };
